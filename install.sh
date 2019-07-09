e84b25bf4dcd2016e68a5b32bc6ff2a59f18ece2
jazer <jazer@tora.tech>
2019-07-09 12:46:17 +0100

    Complete first effort for final script

--- install.sh
+++ install.sh
@@ -1,2 +1,41 @@
 #!/bin/bash
 
+display_usage() { 
+	echo "This script must be run as root!"
+	echo "Usage: sudo $0 [username] [hostname]"
+}
+
+if [ \( $# -ne 2 \) -o \( $EUID -ne 0 \) ]
+then 
+    display_usage
+    exit 1
+fi
+
+printf "Enter $1's Password:" 
+read -s password
+
+apt update && apt -y dist-upgrade && apt -y autoremove && apt -y install vim curl git git-core unattended-upgrades fail2ban ufw
+hostnamectl set-hostname $2
+
+groupadd wheel
+groupadd dev
+
+cat >>/etc/sudoers <<EOL
+#
+# Install script
+%wheel ALL=(ALL) ALL
+Defaults rootpw
+EOL
+
+useradd -m -G wheel,dev -s /bin/bash $1
+echo $1:$password | chpasswd
+
+ufw allow 22
+ufw enable
+ufw status
+
+cat >>/etc/apt/apt.conf.d/10periodic <<EOL
+APT::Periodic::Unattended-Upgrade "1";
+EOL
+
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
